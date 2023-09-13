440f9d265e3b4b60046c00b3e5e15d509f007726
Nikolucas20 <nikolasmora2002@gmail.com>
2023-09-12 21:01:01 -0500

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import { ComidasComponent } from './Componentes/comidas/comidas.component';
 import { DuenosComponent } from './Componentes/duenos/duenos.component';
 import { MascotasComponent } from './Componentes/mascotas/mascotas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenuComponent } from './Componentes/menu/menu.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { ApiComponent } from './Services/api/api.component';
+import { MenuComponent } from './Componentes/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { ApiComponent } from './Services/api/api.component';
     DuenosComponent,
     MascotasComponent,
     MenuComponent,
-    ApiComponent
+
   ],
   imports: [
     BrowserModule,
